97ad74b5d7a7e58a70b7de9bf02acf8bc486e313
ZweHtetAung7 <105115083+ZweHtetAung7@users.noreply.github.com>
2026-03-27 22:34:41 -0400

    Updated final solution

--- Solution.py
+++ Solution.py
@@ -1,8 +1,4 @@
-from Traversals import bfs_path
-import heapq
 from collections import deque
-from Simulator import Simulator
-import sys
 
 class Solution:
 
@@ -12,8 +8,7 @@ class Solution:
         self.graph = graph
         self.info = info
 
-
-    def bfs(self, start, goal):
+    def bfs_parents(self, start):
         queue = deque([start])
         visited = set([start])
         parent = {start: None}
@@ -21,15 +16,15 @@ class Solution:
         while queue:
             curr = queue.popleft()
 
-            if curr == goal:
-                break
-
             for neighbor in self.graph[curr]:
                 if neighbor not in visited:
                     visited.add(neighbor)
                     parent[neighbor] = curr
                     queue.append(neighbor)
 
+        return parent
+
+    def build_path(self, parent, goal):
         if goal not in parent:
             return []
 
@@ -42,7 +37,6 @@ class Solution:
         path.reverse()
         return path
 
-
     def output_paths(self):
         """
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
@@ -50,9 +44,12 @@ class Solution:
         """
         paths, bandwidths, priorities = {}, {}, {}
 
-        for client in self.info["list_clients"]:
-            paths[client] = self.bfs(self.isp, client)
-
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
+
+        parent = self.bfs_parents(self.isp)
+
+        for client in self.info["list_clients"]:
+            paths[client] = self.build_path(parent, client)
+
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
         return (paths, bandwidths, priorities)
